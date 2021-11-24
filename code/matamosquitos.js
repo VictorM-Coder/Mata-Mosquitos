845bed9641fcc010f08f442b7a659a41cbb96495
Victor Martins <martinsvictor6781@gmail.com>
2021-11-24 17:24:10 -0300

    ajustes na estrutura e inicialização da função de spawn

--- code/matamosquitos.js
+++ code/matamosquitos.js
@@ -1,13 +1,33 @@
+if(document.getElementById("btn-jogar") != null){
+    document.getElementById("btn-jogar").addEventListener('click', play)
+}
 
-document.getElementById("btn-jogar").addEventListener('click', play)
+if(document.getElementById("spwam") != null){
+    document.getElementById("spwam").addEventListener('click', spawnMoquisto)
+}
 
+var level
 
 function play(){
     level = document.getElementById("seletor-dificuldade").value
 
+    console.log(level)
+
     if(level != 0){
         window.location.href = "jogo.html"
     }else{
-        window.alert("Escolha uma dificuldade!" + level)
+        window.alert("Escolha uma dificuldade!")
     }
+}
+
+function spawnMoquisto(){
+    /*var mosquito = document.createElement('img')
+    mosquito.setAttribute('src', '_imagens/mosca.png')
+    document.body.appendChild(mosquito)*/
+
+    console.log(randomize(1, 10))
+}
+
+function randomize(min, max){
+    return Math.floor((Math.random() * (max) + (min)))
 }
