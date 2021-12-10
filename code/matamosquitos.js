00a6822fa266f8082c50f659c78ce4742b652979
Victor Martins <martinsvictor6781@gmail.com>
2021-12-09 21:20:40 -0300

    ajustes na estrutura

--- code/matamosquitos.js
+++ code/matamosquitos.js
@@ -1,10 +1,26 @@
 var level
+var totalTimeInSeconds = 14
 
 if(String(window.location.href).indexOf("index.html") != -1){
     document.getElementById("btn-jogar").addEventListener('click', play)
 
 }else if(String(window.location.href).indexOf("jogo.html") != -1){
-    document.getElementById("spwam").addEventListener('click', timer)
+    document.getElementById("spwam").addEventListener('click', spawnMoquisto)
+
+    var time = setInterval(timerDecrement, 1000)
+    var timer = document.getElementById('timer')
+
+    timer.innerText = 15
+
+    function timerDecrement(){
+        timer.innerText = totalTimeInSeconds
+    
+        if(totalTimeInSeconds > 0){
+            totalTimeInSeconds--
+        }else{
+            clearInterval(time)
+        }
+    }
 }
 
 
@@ -12,13 +28,16 @@ function play(){
     var select = (document.getElementById("seletor-dificuldade"))
     level = Number(select.options[select.selectedIndex].value)
 
-    window.alert(level)
-
     if(level != 0){
         window.location.href = "jogo.html"
     }else{
         window.alert("Escolha uma dificuldade!")
+        window.alert(level)
     }
+}
+
+function spawnMosquitoByTimer(){
+    timeSpawn = setInterval(spawnMoquisto, (1000/level))
 }
 
 function spawnMoquisto(){
