cc4f3820129b8dad959d5b808e1ed04998bb939e
Victor Martins <martinsvictor6781@gmail.com>
2021-12-10 11:02:22 -0300

    sistema de spawn de mosquitos em intervalos de tempo
    codificação do sistema de spawn de mosquitos em intervalos de tempo baseado no nível escolhido

--- code/matamosquitos.js
+++ code/matamosquitos.js
@@ -7,20 +7,12 @@ if(String(window.location.href).indexOf("index.html") != -1){
 }else if(String(window.location.href).indexOf("jogo.html") != -1){
     document.getElementById("spwam").addEventListener('click', spawnMoquisto)
 
-    var time = setInterval(timerDecrement, 1000)
-    var timer = document.getElementById('timer')
+    urlGame = String(window.location.href)
+    valueLevel = urlGame.substring((urlGame.indexOf("=") + 1), (urlGame.length))
+    level = Number(valueLevel)
 
-    timer.innerText = 15
-
-    function timerDecrement(){
-        timer.innerText = totalTimeInSeconds
-    
-        if(totalTimeInSeconds > 0){
-            totalTimeInSeconds--
-        }else{
-            clearInterval(time)
-        }
-    }
+    startTime()
+    playSpawnMosquito()
 }
 
 
@@ -29,15 +21,23 @@ function play(){
     level = Number(select.options[select.selectedIndex].value)
 
     if(level != 0){
-        window.location.href = "jogo.html"
+        window.location.href = "jogo.html?level=" + level
     }else{
         window.alert("Escolha uma dificuldade!")
-        window.alert(level)
     }
 }
 
-function spawnMosquitoByTimer(){
-    timeSpawn = setInterval(spawnMoquisto, (1000/level))
+function playSpawnMosquito(){
+    timeSpawn = setInterval(spawnMosquitoByTimer, (1000/level) * 1.5)
+
+    function spawnMosquitoByTimer(){
+        if(totalTimeInSeconds > 0){
+            spawnMoquisto()
+        }else{
+            clearInterval(timeSpawn)
+        }
+    }
+    
 }
 
 function spawnMoquisto(){
@@ -55,4 +55,21 @@ function spawnMoquisto(){
 
 function randomize(min, max){
     return Math.floor((Math.random() * (max) + (min)))
+}
+
+function startTime(){
+    var time = setInterval(timerDecrement, 1000)
+    var timer = document.getElementById('timer')
+
+    timer.innerText = 15
+
+    function timerDecrement(){
+        timer.innerText = totalTimeInSeconds
+    
+        if(totalTimeInSeconds > 0){
+            totalTimeInSeconds--
+        }else{
+            clearInterval(time)
+        }
+    }
 }
